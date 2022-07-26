7ab22f2e4ae87c0a7db36a6b1dd5e7b1dee90fca
Harsh Tomar <harshtomar67tb@gmail.com>
2022-07-26 15:19:45 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.scss'
+
+function App() {
+  return (
+    <div className="App">
+
+        Pokedex
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './index.scss'
+import App from './App'
+
+import { createRoot } from 'react-dom/client'
+
+const root = createRoot(document.getElementById('root'))
+
+root.render(<App/>)
